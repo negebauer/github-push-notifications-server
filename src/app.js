84cafe8232992ad3fbd1a209f69a37e676ef3f46
negebauer <negebauer@uc.cl>
2018-07-25 18:43:24 -0400

    use node env as release

--- src/app.js
+++ src/app.js
@@ -5,16 +5,14 @@ const logger = require('koa-logger')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa-cors')
 const Raven = require('raven')
-const { SENTRY_URL, RELEASE } = require('./env')
+const { SENTRY_URL, NODE_ENV } = require('./env')
 const router = require('./routes')
 require('./db')
 require('./queue')
 require('./queueProcessers')
 require('./notifications')
 
-const r = Raven.config(SENTRY_URL, {
-  release: RELEASE,
-}).install()
+Raven.config(SENTRY_URL, { release: NODE_ENV }).install()
 const app = new Koa()
 
 app.use(cors())
