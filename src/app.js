baf01efd326c2c8ecf20513a8e75604450f0284a
negebauer <negebauer@uc.cl>
2018-07-25 18:39:25 -0400

    configure sentry

--- src/app.js
+++ src/app.js
@@ -4,13 +4,19 @@ const Koa = require('koa')
 const logger = require('koa-logger')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa-cors')
+const Raven = require('raven')
+const { SENTRY_URL, RELEASE } = require('./env')
 const router = require('./routes')
 require('./db')
 require('./queue')
 require('./queueProcessers')
 require('./notifications')
 
+const r = Raven.config(SENTRY_URL, {
+  release: RELEASE,
+}).install()
 const app = new Koa()
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser())
--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ const {
   REDIS_PORT,                         // Redis database
   REDIS_HOST,
   REDIS_PASSWORD,
+  SENTRY_URL,                         // Sentry DSN for error reporting
+  RELEASE,                            // Release version
 } = process.env
 
 const NODE_ENV = NODE_ENV_RAW || 'development'
@@ -27,6 +29,8 @@ module.exports = {
   REDIS_PORT: REDIS_PORT || 6379,
   REDIS_HOST: REDIS_HOST || 'localhost',
   REDIS_PASSWORD: REDIS_PASSWORD || '',
+  SENTRY_URL,
+  RELEASE: RELEASE || 'dev',
 
   NODE_ENV,
   PRODUCTION: NODE_ENV === 'production',
